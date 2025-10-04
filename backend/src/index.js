28a26927ce9f6f5cf4b5027e21ec30aa9d3b8c55
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-10-04 15:52:17 +0530

    Models and user Route created

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import connectDb from './config/db.js';
+import userRouter from './routes/user.route.js'
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/user', userRouter);
 
 app.listen(PORT, ()=>{
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/src/models/job.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["applied", "interviewing", "offered", "rejected"],
+      default: "applied",
+    },
+    applicationDate: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = mongoose.model("Job", jobSchema);
+export default Job;
