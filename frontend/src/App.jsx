51055a2740624de68bcb51c7580062985b92c160
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-10-05 22:34:02 +0530

    Created Routes for different pages using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,36 @@
-
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateJobApplicationPage from "./pages/CreateJobApplicationPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import JobDetailsPage from "./pages/JobDetailsPage";
 
 function App() {
-
   return (
     <div>
-      Hello world
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/create" element={<CreateJobApplicationPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<JobDetailsPage />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
