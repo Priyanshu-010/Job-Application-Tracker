aa4d93ef3f34147e145fd4b52def382a8e6a3212
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-10-05 17:45:26 +0530

    Frontend Setup

---
+++ frontend/src/components/NoJobApplications.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoJobApplications = () => {
+  return (
+    <div>NoJobApplications</div>
+  )
+}
+
+export default NoJobApplications
---
+++ frontend/src/pages/CreateJobApplicationPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateJobApplicationPage = () => {
+  return (
+    <div>CreateJobApplicationPage</div>
+  )
+}
+
+export default CreateJobApplicationPage
