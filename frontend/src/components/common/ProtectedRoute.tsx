0616975e443a3ddb38b6eb2432a3f74a1e8308f1
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-01-05 15:41:34 +0530

    authStore and ProtectedRoute created

---
+++ frontend/src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { useAuthStore } from "../../store/auth.store"
+import { Navigate } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode
+}
+
+const ProtectedRoute = ({children}: Props) =>{
+  const  token = useAuthStore((state) => state.token)
+  if(!token){
+    return <Navigate to="/login" replace/>
+  }
+
+  return <>{children}</>
+}
+
+export default ProtectedRoute
---
+++ frontend/src/store/auth.store.ts
@@ -0,0 +1,23 @@
+import {create} from "zustand"
+import type { User } from "../types/user.types"
+
+type AuthState = {
+  user: User | null;
+  token: string | null;
+  login: (user:User, token: string) => void;
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: null,
+
+  login: (user, token) =>{
+    localStorage.setItem("token", token)
+    set({user, token})
+  },
+  logout: ()=>{
+    localStorage.removeItem("token")
+    set({user: null, token: null})
+  }
+}))
